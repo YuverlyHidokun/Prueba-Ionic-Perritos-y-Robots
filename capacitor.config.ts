6ba4481279e5a9114d4f7b5b4be95f35cb5c991b
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2024-11-22 14:38:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PruebaBimestral',
+  webDir: 'www'
+};
+
+export default config;
